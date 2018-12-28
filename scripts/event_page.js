27456dcdf9cd751eda314dcab1c662f55178d365
jatzz10 <jatinsharma2751997@gmail.com>
2018-12-28 12:42:23 +0530

    Initial commit

---
+++ scripts/event_page.js
@@ -0,0 +1,12 @@
+var menuItem = {
+	"id": "pronounce",
+	"title": "Pronounce",
+	"contexts": ["selection"]
+}
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+	if (clickData.menuItemId == "pronounce" && clickData.selectionText) {
+		chrome.tts.speak(clickData.selectionText, {'rate': 1.0});
+	}
+});
