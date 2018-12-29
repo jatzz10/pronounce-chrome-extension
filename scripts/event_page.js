62c530eda93fc497838728b2f3fbadcec5365b9d
jatzz10 <jatinsharma2751997@gmail.com>
2018-12-29 21:03:34 +0530

    Replaced '==' with '===' operator

--- scripts/event_page.js
+++ scripts/event_page.js
@@ -6,7 +6,7 @@ let menuItem = {
 chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData) {
-	if (clickData.menuItemId == "pronounce" && clickData.selectionText) {
+	if (clickData.menuItemId === "pronounce" && clickData.selectionText) {
 		chrome.tts.speak(clickData.selectionText, {'rate': 1.0});
 	}
 });
